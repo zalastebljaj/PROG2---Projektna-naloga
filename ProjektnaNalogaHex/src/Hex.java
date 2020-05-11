b1294efbf76ccdedf30fef509484352da198288f
User <patrik.mikuz@student.fmf.uni-lj.si>
2020-05-11 23:12:22 +0200

    Narejene datoteke Okno, Platno in osnove le teh, prvi poskusni commit

--- ProjektnaNalogaHex/src/Hex.java
+++ ProjektnaNalogaHex/src/Hex.java
@@ -1,8 +1,13 @@
+import gui.Okno;
 
 public class Hex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Okno okno1 = new Okno(750, 750);
+		
+		okno1.pack();
+		okno1.setVisible(true);
 
 	}
 
